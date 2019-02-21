868186bd20e31f4940b2182ad673f0e5ad968fb1
Rhydex <cr1f3n@gmail.com>
2019-02-21 17:59:44 -0600

    added input and line fields

---
+++ KWIC/Scripts/Kwic/KwicContainer.jsx
@@ -0,0 +1,41 @@
+﻿class KwicContainer extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            something: true
+        };
+    }
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <InputField />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <OutputField />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const OutputField = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+
+
+(function() {
+    ReactDOM.render(
+        <KwicContainer />,
+        document.getElementById('kwic-container')
+    );
+})();
---
+++ KWIC/Scripts/Kwic/LineField.jsx
@@ -0,0 +1,30 @@
+﻿class LineField extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: this.props.list || []
+        }
+    }
+
+    renderList = () => {
+        let tempList = this.state.list;
+        let formattedList = [];
+        tempList.forEach(function(ele) {
+            formattedList.push(<div>{ele}</div>);
+        })
+        if(this.state.list) {
+            return (
+                <div className="well">
+                    {formattedList}
+                </div>
+            )
+        }
+        return (<div></div>);
+    }
+
+    render() {
+        return (
+            this.renderList()
+        )
+    }
+}
