019869d3bc530e14e801d07046e84449ba7c9c2d
Rhydex <cr1f3n@gmail.com>
2019-02-22 11:37:01 -0600

    finished alphabetizing logic for kwic

--- KWIC/Scripts/Kwic/Alphabetizer.jsx
+++ KWIC/Scripts/Kwic/Alphabetizer.jsx
@@ -2,14 +2,37 @@
     constructor(props) {
         super(props);
         this.state= {
-            shift: this.props.shift || [],
             alph: []
         }
+    }
+    componentWillReceiveProps(newProps) {
+        if (this.props == newProps) {return;}
+        this.alphabetizeList(newProps);
+    }
+    alphabetizeList = (newProps) => {
+        let tempList = newProps.shift;
+        tempList.sort((a, b) => {
+            let compare = a[0].toLowerCase().localeCompare(b[0].toLowerCase());
+            let compareTwo = a.toLowerCase().localeCompare(b.toLowerCase());
+            if (compareTwo !=0) {
+                return compareTwo;
+            }
+            if (a < b) {
+                return 1;
+            } else if (a > b) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+        this.setState({
+            alph: tempList
+        });
     }
     render() {
         return(
             <div>
-                <OutputField alph={this.props.shift} />
+                <OutputField alph={this.state.alph} />
             </div>
             
         )
